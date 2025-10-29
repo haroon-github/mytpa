0bff358e35dd4a427e0d0d5f4876dbb75b950fb2
Mario Gonzalez <mario.gonzalez@enterprisedb.com>
2025-10-28 23:45:36 -0300

    Fix copyrights on a few files
    Some typos fixed.

--- lib/tpa/transmogrifiers/pgdproxy_cm.py
+++ lib/tpa/transmogrifiers/pgdproxy_cm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# © Copyright EnterpriseDB UK Limited 205-2025 - All rights reserved.
+# © Copyright EnterpriseDB UK Limited 2015-2025 - All rights reserved.
 
 """
 This transmogrifier changes a PGD-Always-ON cluster running PGD version 5
